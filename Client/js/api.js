8c8fee2ea3f6a0e8b683406187c063d90b9ea31d
dekelPK <dekelkartel@gmail.com>
2025-07-19 12:46:10 +0300

    css + api update

--- Client/js/api.js
+++ Client/js/api.js
@@ -1,2 +1,2 @@
-const api = "https://localhost:7035/api/"; // Local API URL for development
-//"https://proj.ruppin.ac.il/igroup2/test2/tar1/api/"; // API URL
+// "https://localhost:7035/api/"; // Local API URL for development
+const api = "https://proj.ruppin.ac.il/igroup2/test2/tar1/api/"; // API URL
