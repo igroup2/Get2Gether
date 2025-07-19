9113592771711916921496b5e75a7ae04f93949d
dekelPK <dekelkartel@gmail.com>
2025-07-19 13:29:21 +0300

    update dashboard

--- Client/js/api.js
+++ Client/js/api.js
@@ -1,2 +1,2 @@
-// "https://localhost:7035/api/"; // Local API URL for development
-const api = "https://proj.ruppin.ac.il/igroup2/test2/tar1/api/"; // API URL
+const api = "https://localhost:7035/api/"; // Local API URL for development
+//const api = "https://proj.ruppin.ac.il/igroup2/test2/tar1/api/"; // API URL
